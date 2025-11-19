1c461e211e90531218ae48f92f87dca7c3e69d1d
Rendy-Bagus <comebackrenzz@gmail.com>
2025-11-19 12:03:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -101,6 +101,38 @@ const quizData = {
       options: ["192.1.1.0","224.9.1.1", "255.0.0.0"],
       answer: 2
     },
+    {
+      q: "Apa kepanjangan dari IP?",
+      options: ["Internet Program", "Internet Protocol", "Internal Process", "Intranet Packet"],
+      answer: 1
+    },
+    {
+      q: "Berapa bit yang terdapat pada satu alamat IPv4?",
+      options: ["16 bit", "32 bit", "64 bit", "128 bit"],
+      answer: 1
+    },
+    {
+      q: "Berapa banyak oktet pada alamat IPv4?",
+      options: ["2 oktet", "4 oktet", "6 oktet", "8 oktet"],
+      answer: 1
+    },
+    {
+      q: "Contoh IP Address kelas A adalah?",
+      options: ["10.0.0.1", "192.168.1.1", "172.16.0.1", "255.0.0.1"],
+      answer: 0
+    },
+    {
+      q: "Contoh IP Address kelas B adalah?",
+      options: ["172.16.0.1", "10.0.0.1", "192.168.1.1", "255.255.0.1"],
+      answer: 0
+    },
+    {
+      q: "Berapa rentang IP kelas A?",
+      options: ["1.0.0.0 – 126.255.255.255", "128.0.0.0 – 191.255.255.255", "192.168.0.0 – 192.168.255.255", "224.0.0.0 – 239.255.255.255"],
+      answer: 0
+    },
+
+
   ],
   medium: [
     { q: "Berapa subnet mask untuk /26?",
@@ -110,7 +142,49 @@ const quizData = {
     { q: "Berapa host usable dari subnet /30?",
       options: ["0", "2", "4", "6"],
       answer: 1
-    }
+    },
+    {
+      q: "Berapa jumlah host yang bisa digunakan pada subnet dengan prefix /24?",
+      options: ["512 host", "256 host", "254 host", "128 host"],
+      answer: 2
+    },
+    {
+      q: "Jika subnet mask 255.255.255.0, berapa prefix-nya?",
+      options: ["/24", "/25", "/23", "/26"],
+      answer: 0
+    },
+    {
+      q: "IP 192.168.10.1/24 memiliki network address apa?",
+      options: ["192.168.10.0", "192.168.10.1", "192.168.10.255", "192.168.10.128"],
+      answer: 0
+    },
+    {
+      q: "IP 192.168.10.1/24 memiliki broadcast address apa?",
+      options: ["192.168.10.1", "192.168.10.0", "192.168.10.128", "192.168.10.255"],
+      answer: 3
+    },
+    {
+      q: "Jika subnet mask 255.255.255.128, berapa prefix-nya?",
+      options: ["/25", "/26", "/24", "/23"],
+      answer: 0
+    },
+     {
+      q: "Dengan subnet mask /25, berapa host yang bisa digunakan?",
+      options: ["126 host", "128 host", "64 host", "254 host"],
+      answer: 0
+    },
+    {
+      q: "IP 192.168.1.130/25 berada di network berapa?",
+      options: ["192.168.1.192/25", "192.168.1.0/25", "192.168.1.64/25", "192.168.1.128/25"],
+      answer: 3
+    },
+    {
+      q: "Berapa broadcast address dari 192.168.1.128/25?",
+      options: ["192.168.1.254", "192.168.1.128", "192.168.1.255", "192.168.1.129"],
+      answer: 2
+    },
+
+
   ],
   hard: [
     { q: "CIDR /27 memiliki total berapa host?",
@@ -120,7 +194,49 @@ const quizData = {
     { q: "Network ID dari 10.1.5.129/25 adalah?",
       options: ["10.1.5.0", "10.1.5.128", "10.1.5.192", "10.1.5.64"],
       answer: 1
-    }
+    },
+     {
+      q: "Diketahui IP 192.168.10.33/27, network address-nya adalah …",
+      options: ["192.168.10.32", "192.168.10.0", "192.168.10.64", "192.168.10.33"],
+      answer: 0
+    },
+    {
+      q: "Broadcast address dari 192.168.10.33/27 adalah …",
+      options: ["192.168.10.63", "192.168.10.32", "192.168.10.95", "192.168.10.34"],
+      answer: 0
+    },
+    {
+      q: "Berapa host valid untuk subnet /27?",
+      options: ["32", "30", "28", "29"],
+      answer: 1
+    },
+    {
+      q: "IP 192.168.5.77/26 berada di network …",
+      options: ["192.168.5.64/26", "192.168.5.0/26", "192.168.5.128/26", "192.168.5.192/26"],
+      answer: 0
+    },
+    {
+      q: "Broadcast address dari 192.168.5.64/26 adalah …",
+      options: ["192.168.5.126", "192.168.5.64", "192.168.5.255", "192.168.5.127"],
+      answer: 3
+    },
+     {
+      q: "IP 172.16.4.130/20 memiliki network address apa?",
+      options: ["172.16.8.0", "172.16.4.0", "172.16.0.0", "172.16.15.0"],
+      answer: 2
+    },
+    {
+      q: "IP 172.16.4.130/20 memiliki broadcast address apa?",
+      options: ["172.16.15.255", "172.16.4.255", "172.16.31.255", "172.16.0.255"],
+      answer: 0
+    },
+    {
+      q: "Jika prefix /30, berapa host valid maksimal?",
+      options: ["2", "4", "1", "8"],
+      answer: 0
+    },
+
+
   ]
 };
 
